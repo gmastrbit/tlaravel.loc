abd7d42c4198dfdb501b2d2910b5b6f75cc057a3
gmastrbit <gmastrbit@gmail.com>
2018-11-15 18:39:14 +0300

    fix: csrf error

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route:: - фасад
+// get - метод, який відповідає HTTP запиту, для якого ми формуємо наш майбутній маршрут (get, post, put, delete, patch, options)
+
+Route::get('/page', function () {
+    return view('page');
+    //echo "Hello";
+    //var_dump($_ENV);
+    //var_dump(config('app'));
+    //var_dump(Config::get('app'));
+});
+
+// маршрут для методу POST
+Route::post('/comments', function () {
+    var_dump($_POST);
+});
+
+// match дозволяє оборобити різні типи запитів
+//Route::match(['get', 'post'], '/comments', function () {
+//    var_dump($_POST);
+//});
+
+// створення маршруту для всіх типів запитів
+//Route::any('/comments', function () {
+//    var_dump($_POST);
+//});
