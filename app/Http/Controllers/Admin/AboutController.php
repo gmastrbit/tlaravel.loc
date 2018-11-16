6ba62e05de602d934dc0068cc32d2960c0feefd3
gmastrbit <gmastrbit@gmail.com>
2018-11-16 21:29:51 +0300

    chore: response

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+
+// Надаємо доступ до класу Response
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Redirect;
 
 class AboutController extends Controller
 {
@@ -14,7 +19,22 @@ class AboutController extends Controller
     public function show()
     {
         if (view()->exists('default.about')){
+
+            // повертаємо об'єкт класу Response
+            //$view = view('default.about')->withTitle('Hello world')->render();
+            //return (new Response($view))->header('Content-Type','newType');
+
+            // можна повернути JSON рядок
+            //return response()->json(['name' => 'hello', 'name1' => 'hello1']);
+
             return view('default.about')->withTitle('Hello World');
+
+            // віддати файл на скачування
+            //return response()->download('robots.txt', 'mytext.txt');
+
+            // перенаправлення
+            // return redirect('/');
+            //return new RedirectResponse('/articles');
         }
         abort(404);
     }
